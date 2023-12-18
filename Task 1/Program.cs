6e6af56a3d92825dd1a1faace041911594833641
Tatevik Adamyan <tatevadamyan2004@gmail.com>
2023-12-18 21:58:55 +0400

    Task 1 is solved

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int[] array = { 15, 25, 30, 40, 50, 60, 70, 80, 91, 100 };
+
+int count = 0;
+
+foreach (int element in array)
+{
+    if (element >= 20 && element <= 90)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Number of elements in the range [20, 90]: {count}");
